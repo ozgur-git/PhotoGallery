68d788f882199126fcb61ab72ce0a8d5ecba401d
ozgur-git <ozgursevik@gmail.com>
2021-01-07 06:11:15 +0300

    first commit

--- app/src/main/java/com/example/photogallery/PhotoGalleryActivity.java
+++ app/src/main/java/com/example/photogallery/PhotoGalleryActivity.java
@@ -1,7 +1,5 @@
 package com.example.photogallery;
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 
 public class PhotoGalleryActivity extends SingleFragmentActivity {
