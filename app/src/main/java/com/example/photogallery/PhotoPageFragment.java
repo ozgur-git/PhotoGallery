c3c0284435b360a4df4c9fcf6990b05259fab231
ozgur-git <ozgursevik@gmail.com>
2021-01-21 04:35:12 +0300

    first commit

--- app/src/main/java/com/example/photogallery/PhotoPageFragment.java
+++ app/src/main/java/com/example/photogallery/PhotoPageFragment.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
@@ -15,6 +18,7 @@ public class PhotoPageFragment extends VisibleFragment{
 
     private Uri mUri;
     private WebView mWebView;
+    private ProgressBar mProgressBar;
 
     public static PhotoPageFragment newInstance(Uri uri){
         Bundle args=new Bundle();
@@ -39,7 +43,32 @@ public class PhotoPageFragment extends VisibleFragment{
 
         View v=inflater.inflate(R.layout.fragment_photo_page,container,false);
 
+        mProgressBar=v.findViewById(R.id.progressBar);
         mWebView=v.findViewById(R.id.web_view);
+
+        mProgressBar.setMax(100);
+
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                if (newProgress==100){
+                    mProgressBar.setVisibility(View.GONE);
+                } else {
+                    mProgressBar.setVisibility(View.VISIBLE);
+                    mProgressBar.setProgress(newProgress);
+                }
+            }
+
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+//                super.onReceivedTitle(view, title);
+//                getActivity().getActionBar().setSubtitle(title);
+            }
+        });
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.loadUrl(mUri.toString());
 
         return v;
     }
